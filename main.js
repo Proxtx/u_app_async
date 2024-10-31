143b91b279c5a923784a03b3ced33032cd88c9b1
Proxtx <39201896+Proxtx@users.noreply.github.com>
2024-10-31 19:50:48 +0100

    what am i doing??? We have an inbuilt method for this!!

--- main.js
+++ main.js
@@ -1,38 +1,14 @@
 import config from "@proxtx/config";
 import { genCombine } from "@proxtx/combine-rest/request.js";
 import { genModule } from "@proxtx/combine/combine.js";
-
-let api;
+import { execute } from "../../public/api.js";
 
 export class App {
   constructor(config) {
     this.config = config;
   }
 
-  async init() {
-    this.inputApi = await genCombine(
-      this.config.input,
-      "public/api.js",
-      genModule
-    );
-  }
-
   async(action) {
-    (async () => {
-      if (!api) api = await import("../../public/api.js");
-      for (let arg in action.arguments) {
-        action.arguments[arg] = await this.inputApi.resolveInput(
-          config.pwd,
-          action.arguments[arg]
-        );
-      }
-
-      return await api.execute(
-        config.pwd,
-        action.appName,
-        action.method,
-        action.arguments
-      );
-    })();
+    execute(this.config.pwd, action.appName, action.method, action.arguments);
   }
 }
